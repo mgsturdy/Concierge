03beb43cc3ea46a751f0a7602180bce7531fd176
Matt <matt@sturdy.co>
2024-01-17 22:50:37 -0800

    implemented twilio part 7

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Use the Twilio webhook routes
-app.use('/twilio', twilioWebHooks);
+app.use(twilioWebHooks);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
